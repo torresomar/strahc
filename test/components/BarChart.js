de254ddf6f1fa4cee59f21346674596d35e063c5
Omar Eduardo <torres.omar.eduardo@gmail.com>
2015-07-30 17:13:13 -0500

    Include yAxis rendering and rect rendering.

--- test/components/BarChart.js
+++ test/components/BarChart.js
@@ -12,11 +12,13 @@ describe('- BarChart component', function(){
             {name: 'C',value: 800},
             {name: 'D',value: 100}
         ];
+        var megaColors = ["#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#2c3e50", "#f1c40f", "#e67e22", "#e74c3c", "#ecf0f1", "#95a5a6", "#f39c12", "#d35400", "#c0392b", "#bdc3c7", "#7f8c8d"];
         var renderedComponent = TestUtils.renderIntoDocument(
             <div style={{width: 600, height: 400}}>
                 <BarChart
                     height={400}
                     width={600}
+                    colors={megaColors}
                     data={data}/>
             </div>
         );
