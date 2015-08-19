ba7e77e6f3820f5c1e6cdccbf6ffd58154fe9e58
Omar Eduardo <torres.omar.eduardo@gmail.com>
2015-08-19 17:10:39 -0500

    Initial donut chart configurations.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,11 +16,7 @@ var Charts = React.createClass({
         var data = [];
         var size = 30;
         while(size--){
-            // var neg = -1;
             var neg = getRandomInt(-1,1);
-            // if(size % 2 === 0){
-            //     neg = 1;
-            // }
             data.push({
                 name: size + '',
                 date: new Date(2012,1,size),
@@ -74,38 +70,17 @@ var Charts = React.createClass({
                         <button onClick={this.emptyData} className="btn btn-danger" type="submit">Remove Data</button>
                     </div>
                     <div className='col-sm-6'>
-                        <h4>Using the component:</h4>
-                        <pre>
-                            &lt;BarChart
-                                height=&#123;400&#125;
-                                color=&#123;'#ec971f'&#125;
-                                xAxis=&#123;xAxis&#125;
-                                margin=&#123;margin&#125;
-                                divisions=&#123;[0,45,-30]&#125;
-                                valueKey='name'
-                                data=&#123;data&#125;/&gt;
-                        </pre>
-                        <p>Whenever this code is run, it recomputes the data join and maintains the desired correspondence between elements and data. If the new dataset is smaller than the old one, the surplus elements end up in the <i>exit</i> selection and get removed. If the new dataset is larger, the surplus data ends up in the <i>enter</i> selection and new nodes are added. If the new dataset is exactly the same size, then all the elements are simply updated with new positions, and no elements are added or removed.</p>
-                        <p>Thinking with joins means your code is more <i>declarative</i>: you handle these three states without any branching (<code class="javascript"><span class="keyword">if</span></code>) or iteration (<code class="javascript"><span class="keyword">for</span></code>). Instead you describe how elements should correspond to data. If a given <i>enter</i>, <i>update</i> or <i>exit</i> selection happens to be empty, the corresponding code is a no-op.</p>
-                        <p>Joins also let you target operations to specific states, if needed. For example, you can set constant attributes (such as the circle’s radius, defined by the <code class="javascript"><span class="string">"r"</span></code> attribute) on enter rather than update. By reselecting elements and minimizing DOM changes, you vastly improve rendering performance! Similarly, you can target animated transitions to specific states. For example, for entering circles to expand-in:</p>
-                        <ol>
-                            <li>
-                                <p>First, <code class="javascript">svg.selectAll(<span class="string">"circle"</span>)</code> returns a new empty selection, since the SVG container was empty. The parent node of this selection is the SVG container.
-                                </p>
-                            </li>
-                            <li>
-                                <p>This selection is then joined to an array of data, resulting in three new selections that represent the three possible states: <i>enter</i>, <i>update</i>, and <i>exit</i>. Since the selection was empty, the update and exit selections are empty, while the enter selection contains a placeholder for each new datum.
-                                </p>
-                            </li>
-                            <li>
-                                <p>The update selection is returned by <a href="https://github.com/mbostock/d3/wiki/Selections#wiki-data">selection.data</a>, while the enter and exit selections hang off the update selection; <a href="https://github.com/mbostock/d3/wiki/Selections#wiki-enter">selection.enter</a> thus returns the enter selection.
-                                </p>
-                            </li>
-                            <li>
-                                <p>The missing elements are added to the SVG container by calling <a href="https://github.com/mbostock/d3/wiki/Selections#wiki-append">selection.append</a> on the enter selection. This appends a new circle for each data point to the SVG container.
-                                </p>
-                            </li>
-                        </ol>
+                        <FillerHTML/>
+                    </div>
+                </div>
+                <hr/>
+                <div className='row'>
+                    <div className='col-sm-6'>
+                        <FillerHTML/>
+                    </div>
+                    <div className='col-sm-6'>
+                        <h4>Donut Chart</h4>
+                        <DonutChart/>
                     </div>
                 </div>
             </div>
@@ -113,4 +88,45 @@ var Charts = React.createClass({
     }
 });
 
+var FillerHTML = React.createClass({
+    render: function(){
+        return(
+            <div>
+                <h4>Using the component:</h4>
+                <pre>
+                    &lt;BarChart
+                    height=&#123;400&#125;
+                    color=&#123;'#ec971f'&#125;
+                    xAxis=&#123;xAxis&#125;
+                    margin=&#123;margin&#125;
+                    divisions=&#123;[0,45,-30]&#125;
+                    valueKey='name'
+                    data=&#123;data&#125;/&gt;
+                </pre>
+                <p>Whenever this code is run, it recomputes the data join and maintains the desired correspondence between elements and data. If the new dataset is smaller than the old one, the surplus elements end up in the <i>exit</i> selection and get removed. If the new dataset is larger, the surplus data ends up in the <i>enter</i> selection and new nodes are added. If the new dataset is exactly the same size, then all the elements are simply updated with new positions, and no elements are added or removed.</p>
+                <p>Thinking with joins means your code is more <i>declarative</i>: you handle these three states without any branching (<code class="javascript"><span class="keyword">if</span></code>) or iteration (<code class="javascript"><span class="keyword">for</span></code>). Instead you describe how elements should correspond to data. If a given <i>enter</i>, <i>update</i> or <i>exit</i> selection happens to be empty, the corresponding code is a no-op.</p>
+                <p>Joins also let you target operations to specific states, if needed. For example, you can set constant attributes (such as the circle’s radius, defined by the <code class="javascript"><span class="string">"r"</span></code> attribute) on enter rather than update. By reselecting elements and minimizing DOM changes, you vastly improve rendering performance! Similarly, you can target animated transitions to specific states. For example, for entering circles to expand-in:</p>
+                <ol>
+                    <li>
+                        <p>First, <code class="javascript">svg.selectAll(<span class="string">"circle"</span>)</code> returns a new empty selection, since the SVG container was empty. The parent node of this selection is the SVG container.
+                            </p>
+                        </li>
+                        <li>
+                            <p>This selection is then joined to an array of data, resulting in three new selections that represent the three possible states: <i>enter</i>, <i>update</i>, and <i>exit</i>. Since the selection was empty, the update and exit selections are empty, while the enter selection contains a placeholder for each new datum.
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>The update selection is returned by <a href="https://github.com/mbostock/d3/wiki/Selections#wiki-data">selection.data</a>, while the enter and exit selections hang off the update selection; <a href="https://github.com/mbostock/d3/wiki/Selections#wiki-enter">selection.enter</a> thus returns the enter selection.
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>The missing elements are added to the SVG container by calling <a href="https://github.com/mbostock/d3/wiki/Selections#wiki-append">selection.append</a> on the enter selection. This appends a new circle for each data point to the SVG container.
+                                        </p>
+                                    </li>
+                                </ol>
+
+                            </div>
+        );
+    }
+});
 React.render(<Charts/>, document.getElementById('react-strahc'));
