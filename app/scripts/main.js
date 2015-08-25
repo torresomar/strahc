ca66ac61502313615342beb680254e2a9bea71fd
Christian Consuelo <cconsuelo@intelimetrica.com>
2015-08-25 13:00:39 -0500

    Add LineChart Component

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,7 @@ var React = require('react');
 var d3 = require('d3');
 
 var BarChart = require('./../../lib/BarChart');
+var LineChart = require('./../../lib/LineChart');
 
 var Charts = React.createClass({
     getInitialState: function(){
@@ -12,9 +13,31 @@ var Charts = React.createClass({
         };
     },
     changeData: function(){
+        var totalArrays = [];
         var data = [];
-        var size = 1000;
-        while(size--){
+        var size = 10;
+        var i = 0;
+        var numLines = 5;
+
+        while(numLines--) {
+            while(i < size) {
+                var neg = -1;
+                if(i % 2 === 0){
+                    neg = 1;
+                }
+                data.push({
+                    name: String.fromCharCode(97 + i),
+                    date: new Date(2012,1,(i+1)),
+                    value: getRandomInt(0, 100) * neg,
+                    ratio: getRandomInt(0, 1010000) * neg
+                });
+                i++;
+            }
+            totalArrays.push(data);
+            data = [];
+            i = 0;
+        }
+        while(size--) {
             var neg = -1;
             if(size % 2 === 0){
                 neg = 1;
@@ -23,11 +46,12 @@ var Charts = React.createClass({
                 name: String.fromCharCode(97 + size),
                 date: new Date(2012,1,size),
                 value: getRandomInt(0, 100) * neg,
-                ratio: getRandomInt(0, 1000) * neg
+                ratio: getRandomInt(0, 1010000) * neg
             });
         }
         this.setState({
-            data: data
+            dataLine: totalArrays,
+            dataBar: data,
         });
         function getRandomInt(min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -38,7 +62,8 @@ var Charts = React.createClass({
     },
     emptyData: function(){
         this.setState({
-            data: []
+            dataLine: [],
+            dataBar: [],
         });
     },
     render: function(){
@@ -56,17 +81,20 @@ var Charts = React.createClass({
         }
         return(
             <div className='col-sm-12'>
+                {/*
                 <div className='row'>
                     <div className='col-sm-6'>
                         <h4>Bar Chart</h4>
                         <BarChart
+                            scale='linear'
                             height={400}
                             color={'#ec971f'}
+                            colors={megaColors}
                             xAxis={xAxis}
                             margin={margin}
                             divisions={[0,45,-30]}
                             valueKey='name'
-                            data={this.state.data}/>
+                            data={this.state.dataBar}/>
                         <hr/>
                         <button onClick={this.changeData} className="btn btn-warning" type="submit">Load Data</button>
                         <button onClick={this.emptyData} className="btn btn-danger" type="submit">Remove Data</button>
@@ -106,6 +134,58 @@ var Charts = React.createClass({
                         </ol>
                     </div>
                 </div>
+                <div className='row'>
+                    <div className='col-sm-6'>
+                        <h4>Bar Chart</h4>
+                        <LineChart
+                            scale='time'
+                            height={400}
+                            colors={megaColors}
+                            xAxis={xAxis}
+                            margin={margin}
+                            divisions={[0,45,-30]}
+                            valueKey='date'
+                            data={this.state.dataLine}/>
+                        <hr/>
+                        <button onClick={this.changeData} className="btn btn-warning" type="submit">Load Data</button>
+                        <button onClick={this.emptyData} className="btn btn-danger" type="submit">Remove Data</button>
+                    </div>
+                    <div className='col-sm-6'>
+                        <h4>Using the component:</h4>
+                        <code>
+                            &lt;LineChart
+                                height=&#123;400&#125;
+                                colors=&#123;["#1abc9c", "#2ecc71", "#3498db"]&#125;
+                                xAxis=&#123;xAxis&#125;
+                                margin=&#123;margin&#125;
+                                divisions=&#123;[0,45,-30]&#125;
+                                valueKey='name'
+                                data=&#123;data&#125;/&gt;
+                        </code>
+                        <p>Whenever this code is run, it recomputes the data join and maintains the desired correspondence between elements and data. If the new dataset is smaller than the old one, the surplus elements end up in the <i>exit</i> selection and get removed. If the new dataset is larger, the surplus data ends up in the <i>enter</i> selection and new nodes are added. If the new dataset is exactly the same size, then all the elements are simply updated with new positions, and no elements are added or removed.</p>
+                        <p>Thinking with joins means your code is more <i>declarative</i>: you handle these three states without any branching (<code class="javascript"><span class="keyword">if</span></code>) or iteration (<code class="javascript"><span class="keyword">for</span></code>). Instead you describe how elements should correspond to data. If a given <i>enter</i>, <i>update</i> or <i>exit</i> selection happens to be empty, the corresponding code is a no-op.</p>
+                        <p>Joins also let you target operations to specific states, if needed. For example, you can set constant attributes (such as the circle’s radius, defined by the <code class="javascript"><span class="string">"r"</span></code> attribute) on enter rather than update. By reselecting elements and minimizing DOM changes, you vastly improve rendering performance! Similarly, you can target animated transitions to specific states. For example, for entering circles to expand-in:</p>
+                        <ol>
+                            <li>
+                                <p>First, <code class="javascript">svg.selectAll(<span class="string">"circle"</span>)</code> returns a new empty selection, since the SVG container was empty. The parent node of this selection is the SVG container.
+                                </p>
+                            </li>
+                            <li>
+                                <p>This selection is then joined to an array of data, resulting in three new selections that represent the three possible states: <i>enter</i>, <i>update</i>, and <i>exit</i>. Since the selection was empty, the update and exit selections are empty, while the enter selection contains a placeholder for each new datum.
+                                </p>
+                            </li>
+                            <li>
+                                <p>The update selection is returned by <a href="https://github.com/mbostock/d3/wiki/Selections#wiki-data">selection.data</a>, while the enter and exit selections hang off the update selection; <a href="https://github.com/mbostock/d3/wiki/Selections#wiki-enter">selection.enter</a> thus returns the enter selection.
+                                </p>
+                            </li>
+                            <li>
+                                <p>The missing elements are added to the SVG container by calling <a href="https://github.com/mbostock/d3/wiki/Selections#wiki-append">selection.append</a> on the enter selection. This appends a new circle for each data point to the SVG container.
+                                </p>
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                */}
             </div>
         );
     }
