1eac2b1c2781c86e65434e8a3f539f5319103380
Omar Eduardo <torres.omar.eduardo@gmail.com>
2015-08-31 18:57:05 -0500

    Add legend formatter.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -53,6 +53,13 @@ var Charts = React.createClass({
         element.click();
         document.body.removeChild(element);
     },
+    legendFormatter: function(d){
+        var strBuffer = '';
+        strBuffer += '<h4>' + d.name + '</h4>';
+        strBuffer += '<hr/>';
+        strBuffer += '<h4>' + d.value + '</h4>';
+        return strBuffer;
+    },
     render: function(){
         var megaColors = ["#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#2c3e50", "#f1c40f", "#e67e22", "#e74c3c", "#ecf0f1", "#95a5a6", "#f39c12", "#d35400", "#c0392b", "#bdc3c7", "#7f8c8d"];
         var xAxis = {
@@ -112,7 +119,9 @@ var Charts = React.createClass({
                         <DonutChart
                             thickness={40}
                             tooltip={false}
-                            data={donutData}/>
+                            data={donutData}
+                            legendFormatter={this.strBuffer}
+                        />
                     </div>
                 </div>
             </div>
